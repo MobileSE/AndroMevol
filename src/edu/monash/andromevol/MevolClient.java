415908862c25e5e722fdbe7aed8a0f093e8defc0
Patrick Liu <liupeifox@gmail.com>
2021-08-09 09:40:35 +1000

    extract update

--- src/edu/monash/andromevol/MevolClient.java
+++ src/edu/monash/andromevol/MevolClient.java
@@ -1,15 +1,23 @@
 package edu.monash.andromevol;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+
 import edu.monash.utils.FileInOutUtils;
+import edu.monash.utils.DexHunter;
 
 public class MevolClient {
 
 	public static void main(String[] args) {
-		mining(args);
+		try {
+			mining(args);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public static void mining(String[] args) {
+	public static void mining(String[] args) throws IOException {
 		String platforms = args[0];
 		String frameworkPath = args[1];
 		String outBase = "./res";
@@ -36,7 +44,10 @@ public class MevolClient {
 		methodOutPath += File.separator + "methods.txt";
 		fieldOutPath += File.separator + "fields.txt";
 		MethodFieldExtractor extractor = new MethodFieldExtractor();
-		extractor.transform(frameworkPath, platforms, apiLevel);
+		Set<String> additionalDexes = new DexHunter(frameworkPath).hunt();
+		for (String dex : additionalDexes) {
+			extractor.transform(dex, platforms, apiLevel);
+		}
 		FileInOutUtils.writeToFile(extractor.apimethods, methodOutPath);
 		FileInOutUtils.writeToFile(extractor.apifields, fieldOutPath);
 	}
